85f3e874341ead1d294b2f9dfcf39eb880eaf628
borislaw.sotnikow <borislaw.sotnikow@kuhfussonline.com>
2023-09-25 13:51:36 +0200

    profile 7zip
    added 7zip path + aliases

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,8 +4,16 @@ Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
+# env
+$env:Path += ";$env:ProgramFiles\7-Zip"
+
 # Aliases
 set-alias vim nvim
 set-alias vi nvim
+
+Function 7zipA { 7z a }
+set-alias zip 7zipA
+Function 7zipX { 7z x }
+set-alias unzip 7zipX
 
 aliae init pwsh | Invoke-Expression
