a1ac8f377ff54be7ba59814f82536661f4758b1d
cethien <borislaw.sotnikow@gmx.de>
2023-12-05 20:24:51 +0100

    feat(scripts): update cli style

--- scripts/sdk/go/update.sh
+++ scripts/sdk/go/update.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-(cd $HOME && \
+CURRENT_GO_VERISON="$(go version | cut -d " " -f 3)"
+LATEST_GO_VERSION="$(curl --silent https://go.dev/VERSION?m=text | head -n 1)"
+
+if [[ "${CURRENT_GO_VERISON}" == "${LATEST_GO_VERSION}" ]]; then
+    echo "go is up to date"
+    return
+fi
+
+(cd "$HOME" && \
 curl -fsSL https://s.id/golang-linux | bash -s && \
 rm -rf go*.tar.gz)
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-sudo nala update && \
+COMMAND=$1
+ARG=$2
+
+if [ -z "$COMMAND" ]; then
+    sudo nala update && \
     sudo nala upgrade -y && \
-    curl -fsSL https://ohmyposh.dev/install.sh | bash -s -- -d $HOME/.local/bin && \
-    curl -fsSL https://aliae.dev/install.sh | bash -s -- -d $HOME/.local/bin
+    curl -fsSL https://ohmyposh.dev/install.sh | bash -s -- -d "$HOME"/.local/bin && \
+    curl -fsSL https://aliae.dev/install.sh | bash -s -- -d "$HOME"/.local/bin
+    return
+fi
+
+if [[ $COMMAND == "sdk" ]]; then
+    SDK=$ARG
+
+    if [[ -z $SDK ]]; then
+        for s in "$HOME"/scripts/sdk/**/*.sh; do
+            . "$s"
+        done
+        return
+    fi
+
+    . "$HOME"/scripts/sdk/"$SDK"/update.sh
+    return
+fi
+
