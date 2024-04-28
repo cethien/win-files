a66012d21098f129ba7f5ca4f8ca2744b6cf838d
cethien <borislaw.sotnikow@gmx.de>
2024-04-28 15:37:33 +0200

    chore: moved scripts to own repo

--- scripts/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# -eq 1 ]; then
-    mkdir "$1" && cd "$_" && git init && touch README.md .gitignore
-    return 0
-else
-    echo "Illegal number of parameters"
-    return 1
-fi
--- scripts/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo nala update &&
-    sudo nala upgrade -y &&
-    sudo nala autoremove -y
-
-nix-channel --update
-(cd "$HOME"/.config/home-manager && nix flake update)
